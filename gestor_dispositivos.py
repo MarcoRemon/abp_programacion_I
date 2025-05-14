fd2ba31e87b580f5005ecc15936ba523eb12d5fd
MarcoRemon <marcoremonwrk@gmail.com>
2025-05-14 18:35:46 -0300

    [Primer_commit]

---
+++ gestor_dispositivos.py
@@ -0,0 +1,270 @@
+dispositivos = {}
+
+automatizaciones = {}
+
+TIPOS_VALIDOS = ("AIRE_ACONDICIONADO", "VENTILADOR",
+                 "ENCENDIBLES")
+MODOS_AC = ("VENTILADOR", "FRIO", "CALOR", "HUMEDAD")
+
+
+def automatizar_dispositivo(tipo):
+    if tipo == 1:
+        automatizar_aire_acondicionado()
+    if tipo == 2:
+        automatizar_ventilador()
+    if tipo == 3:
+        automatizar_encendido_apagado()
+
+
+def automatizar_encendido_apagado():
+    arranque = ""
+    apagado = ""
+    texto_estado = "ENCENDIDO"
+    if check_dispositivos_por_tipo("ENCENDIBLES"):
+        mostrar_dispositivos_por_tipo("ENCENDIBLES")
+    else:
+        print("No existen dispositivos del tipo.")
+        return False
+
+    identificador = int(input("Indique que dispositivo quiere automatizar: "))
+
+    print("Indique un horario de inicio de la rutina:")
+    inicio = input()
+    print("Indique el estado deseado del dispositivo ( ENCENDIDO (Y) /APAGADO (X) ):")
+    estado = input()
+    while str.upper(estado) not in ("X", "Y"):
+        print("Indique el estado deseado del dispositivo ( ENCENDIDO (Y) /APAGADO (X) ):")
+        estado = input()
+
+    if estado == "X":
+        texto_estado = "APAGADO"
+        print(
+            "Si desea agregar un horario de encendido indiquelo a continuacion. De lo contrario deje vacio el espacio.")
+        arranque = input()
+    else:
+        print(
+            "Si desea agregar un horario de apagado indiquelo a continuacion. De lo contrario deje vacio el espacio.")
+        apagado = input()
+
+    automatizaciones[identificador] = {
+        "Horario de inicio de la rutina": inicio,
+        "Estado del dispositivo": texto_estado,
+        "Horario arranque de dispositivo": arranque,
+        "Horario apagado de dispositivo": apagado,
+    }
+
+    return True
+
+
+def automatizar_aire_acondicionado():
+    arranque = ""
+    apagado = ""
+    velocidad = 0
+    temperatura = 0
+    modo = ""
+    texto_estado = "ENCENDIDO"
+    if check_dispositivos_por_tipo("AIRE_ACONDICIONADO"):
+        mostrar_dispositivos_por_tipo("AIRE_ACONDICIONADO")
+    else:
+        print("No existen dispositivos del tipo.")
+        return False
+    identificador = int(input("Indique que dispositivo quiere automatizar: "))
+
+    print("Indique un horario de inicio de la rutina:")
+    inicio = input()
+    print("Indique el estado deseado del dispositivo ( ENCENDIDO (Y) /APAGADO (X) ):")
+    estado = input()
+    while str.upper(estado) not in ("X", "Y"):
+        print("Indique el estado deseado del dispositivo ( ENCENDIDO (Y) /APAGADO (X) ):")
+        estado = input()
+
+    if estado == "X":
+        texto_estado = "APAGADO"
+        print(
+            "Si desea agregar un horario de encendido indiquelo a continuacion. De lo contrario deje vacio el espacio.")
+        arranque = input()
+    else:
+        print("Indique la velocidad del dispositivo con un valor entre 1 y 3 inclusive:")
+        velocidad = int(input())
+        if 0 < velocidad > 4:
+            while 0 < velocidad > 4:
+                print("Opcion invalida.")
+                print("Indique la velocidad del dispositivo con un valor entre 1 y 3 inclusive:")
+                velocidad = int(input())
+        print(
+            "Si desea agregar un horario de apagado indiquelo a continuacion. De lo contrario deje vacio el espacio.")
+        apagado = input()
+        print("Ingrese el modo de aire acondicionado: ( (1) VENTILADOR,(2) FRIO, (3) CALOR, (4) HUMEDAD) ")
+        modo_input = int(input())
+        if modo_input < 0 or modo_input > 4:
+            while modo_input < 0 or modo_input > 4:
+                print("Opcion invalida")
+                print("Ingrese el modo de aire acondicionado: ((1) VENTILADOR,(2) FRIO, (3) CALOR, (4) HUMEDAD) ")
+                if modo_input == 1:
+                    modo = "VENTILADOR"
+                elif modo_input == 2:
+                    modo = "FRIO"
+                elif modo_input == 3:
+                    modo = "CALOR"
+                elif modo_input == 4:
+                    modo = "HUMEDAD"
+        print("Ingrese una temperatura entre 15-38 (Celcius): ")
+        temperatura = int(input())
+        if temperatura < 15 or temperatura > 38:
+            while temperatura < 15 or temperatura > 38:
+                print("Opcion invalida")
+                print("Ingrese una temperatura entre 15-38 (Celcius): ")
+                temperatura = int(input())
+
+    automatizaciones[identificador] = {
+        "Horario de inicio de la rutina": inicio,
+        "Estado del dispositivo": texto_estado,
+        "Horario arranque de dispositivo": arranque,
+        "Horario apagado de dispositivo": apagado,
+        "Velocidad ventilador": velocidad,
+        "Modo": modo,
+        "Temperatura": temperatura
+    }
+
+    return True
+
+
+def automatizar_ventilador():
+    arranque = ""
+    apagado = ""
+    velocidad = 0
+    texto_estado = "ENCENDIDO"
+    if check_dispositivos_por_tipo("VENTILADOR"):
+        mostrar_dispositivos_por_tipo("VENTILADOR")
+    else:
+        print("No existen dispositivos del tipo.")
+        return False
+    identificador = int(input("Indique que dispositivo quiere automatizar: "))
+
+    print("Indique un horario de inicio de la rutina:")
+    inicio = input()
+
+    print("Indique el estado deseado del dispositivo ( ENCENDIDO (Y) /APAGADO (X) ):")
+
+    estado = input()
+
+    while str.upper(estado) not in ("X", "Y"):
+        print("Indique el estado deseado del dispositivo ( ENCENDIDO (Y) /APAGADO (X) ):")
+
+        estado = input()
+
+    if estado == "X":
+        texto_estado = "APAGADO"
+        print(
+            "Si desea agregar un horario de encendido indiquelo a continuacion. De lo contrario deje vacio el espacio.")
+        arranque = input()
+    else:
+        print("Indique la velocidad del dispositivo con un valor entre 1 y 3 inclusive:")
+        velocidad = int(input())
+        if velocidad < 0 or velocidad > 4:
+            while velocidad < 0 or velocidad > 4:
+                print("Opcion invalida.")
+                print("Indique la velocidad del dispositivo con un valor entre 1 y 3 inclusive:")
+                velocidad = int(input())
+        print(
+            "Si desea agregar un horario de apagado indiquelo a continuacion. De lo contrario deje vacio el espacio.")
+        apagado = input()
+
+    automatizaciones[identificador] = {
+        "Horario de inicio de la rutina": inicio,
+        "Estado del dispositivo": texto_estado,
+        "Horario arranque de dispositivo": arranque,
+        "Horario apagado de dispositivo": apagado,
+        "Velocidad ventilador": velocidad
+    }
+
+    return True
+
+
+def agregar_dispositivo(nombre, tipo):
+    tipo_dispositivo = ""
+    if tipo > 0 and tipo < 3:
+        if tipo == 1:
+            tipo_dispositivo = "AIRE_ACONDICIONADO"
+        if tipo == 2:
+            tipo_dispositivo = "VENTILADOR"
+        if tipo == 3:
+            tipo_dispositivo = "ENCENDIBLES"
+    else:
+        print("Opcion invalida")
+    if check_dispositivo_existe_por_nombre(str.upper(nombre)) or len(nombre) < 1:
+        print("El dispositivo ya existe o el valor ingresado como nombre es invalido")
+        return False
+    else:
+        dispositivo = {
+            "Nombre": str.upper(nombre),
+            "Estado": "Encendido",
+            "Tipo": tipo_dispositivo
+        }
+        dispositivos[len(dispositivos) + 1] = dispositivo
+
+
+def check_dispositivo_existe_por_nombre(nombre):
+    for disp in dispositivos.values():
+        for valor in disp.values():
+            if str.upper(nombre) == valor:
+                return True
+
+
+def mostrar_dispositivos():
+    for id_disp, disp in dispositivos.items():
+        print(f"Identificador del dispositivo: {id_disp}.")
+        for clave, valor in disp.items():
+            print(f"  {clave}: {valor}")
+        print()
+
+
+def buscar_dispositivo_por_nombre(nombre):
+    for id_disp, disp in dispositivos.items():
+        if disp['Nombre'] == str.upper(nombre):
+            print(f"Identificador del dispositivo: {id_disp}. "
+                  f"Nombre:{disp['Nombre']}. "
+                  f"Estado:{disp['Estado']}. "
+                  f"Tipo:{disp['Tipo']}.")
+
+
+def mostrar_dispositivos_por_tipo(tipo):
+    for id_disp, disp in dispositivos.items():
+        if disp['Tipo'] == str.upper(tipo):
+            print(f"Identificador del dispositivo: {id_disp}. Nombre:{disp['Nombre']}.")
+
+
+def check_dispositivos_por_tipo(tipo):
+    for id_disp, disp in dispositivos.items():
+        if disp['Tipo'] == str.upper(tipo):
+            return True
+    return False
+
+
+def eliminar_dispositivo():
+    mostrar_dispositivos()
+    identificador = int(input("Ingrese el Identificador del dispositivo: "))
+    if not identificador > len(dispositivos) or identificador <= 0:
+        dispositivo = dispositivos.pop(identificador)
+        print(f"El dispositivo '{dispositivo['Nombre']}' y su rutina fueron eliminados correctamente.")
+        automatizaciones.pop(identificador)
+    else:
+        print("Valor invalido.")
+
+
+def mostrar_automatizaciones():
+    for key, values in automatizaciones.items():
+        print(f"Dispositivo:{key}.")
+        for parametro, automatizacion in values:
+            print("Rutina: ")
+            print(f"{parametro},{automatizacion}")
+
+
+def eliminar_automatizacion():
+    mostrar_dispositivos()
+    identificador = int(input("Ingrese el Identificador del dispositivo cuya rutina desea eliminar: "))
+    if not identificador > len(dispositivos) or identificador <= 0:
+        automatizaciones.pop(identificador)
+        print("Rutina eliminada.")
+    else:
+        print("Valor invalido.")
---
+++ main.py
@@ -0,0 +1,66 @@
+import gestor_dispositivos
+
+
+def menu():
+    print("Menu:")
+    print("1:Registrar dispositivo:")
+    print("2:Mostrar dispositivos:")
+    print("3:Buscar dispositivo:")
+    print("4:Automatizar dispositivo:")
+    print("5:Eliminar automatizacion")
+    print("6:Desconectar dispositivo:")
+    print("7:Salir:")
+
+
+aplicacion_ejecutando = True
+
+while aplicacion_ejecutando:
+
+    print()
+    menu()
+    print()
+
+    try:
+        opcion = int(input("Elija una opcion:"))
+
+        if opcion < 1 or opcion > 7:
+            print("Opcion invalida.")
+        else:
+            if opcion == 1:
+                nombre = input("Indique el nombre del dispositivo: ")
+                tipo_input = int(input(
+                    "¿Que tipo de dispositivo es?: "
+                    "1) Aire acondicionado. "
+                    "2) Ventilador (Pie, Techo). "
+                    "3) Encendible (Luces, Computadores personales, cafeteras): "))
+                if 0 < tipo_input < 3:
+                    gestor_dispositivos.agregar_dispositivo(nombre, tipo_input)
+                    print(f"El dispositivo {nombre} fue registrado correctamente.")
+                else:
+                    print("Opcion invalida")
+
+            if opcion == 2:
+                gestor_dispositivos.mostrar_dispositivos()
+            if opcion == 3:
+                nombre = input("Indique el nombre del dispositivo: ")
+                gestor_dispositivos.buscar_dispositivo_por_nombre(nombre)
+            if opcion == 4:
+                tipo_input = int(input(
+                    "¿Que tipo de dispositivo es?: "
+                    "1) Aire acondicionado. "
+                    "2) Ventilador (Pie, Techo). "
+                    "3) Encendible (Luces, Computadores personales, cafeteras): "))
+                if 0 < tipo_input <= 3:
+                    gestor_dispositivos.automatizar_dispositivo(tipo_input)
+                else:
+                    print("Opcion invalida")
+        if opcion == 5:
+            gestor_dispositivos.eliminar_automatizacion()
+        if opcion == 6:
+            gestor_dispositivos.eliminar_dispositivo()
+        if opcion == 7:
+            print("Cerrando aplicacion.")
+            aplicacion_ejecutando = False
+
+    except ValueError:
+        print("El valor debe ser numerico")
